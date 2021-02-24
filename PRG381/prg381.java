2447777a8f433df32defd054b0d638f1d33c199f
FIRST_NAME LAST_NAME <576341@student.belgiumcampus.ac.za>
2021-02-24 11:31:58 +0200

    Added new print line

--- PRG381/prg381.java
+++ PRG381/prg381.java
@@ -6,8 +6,11 @@ public class prg381
 public static void main(String[] args) 
 
 {
-    System.out.println("My 1st program on GIT");
+    
+    System.out.println("My first program on GIT");
+    System.out.println("=======================");
 
 }
 
 }
+ 
